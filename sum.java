517fcd1563cbfa1ccb121db99da5ac49c7c4335a
Anshikagupta1306 <anshika13064@gmail.com>
2025-01-14 23:52:25 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int sum=A+B;
+        System.out.println("sum is:"+sum);
+    }
+}
