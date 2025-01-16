48a15460a83013b9faeb17e6ca45fdaa7f6fce18
Anshikagupta1306 <anshika13064@gmail.com>
2025-01-16 14:42:57 +0530

    Add files via upload

---
+++ star.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class star {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number of lines:");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
