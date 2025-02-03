190309c07d706216b9db7aae35e7b311ff990d60
Anshikagupta1306 <anshika13064@gmail.com>
2025-02-03 09:44:53 +0530

    Add files via upload

---
+++ stocks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class stocks {
+    public static int buyandsellstocks(int prices[]){
+        int maxprofit=0;
+        int buyprice=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(buyprice<prices[i]){
+                int profit=prices[i]-buyprice;
+                maxprofit=Math.max(maxprofit,profit);
+            }
+            else{
+              buyprice=prices[i];
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(buyandsellstocks(prices));
+    }
+}
