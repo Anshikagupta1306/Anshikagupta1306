512b28314cc0ad2d6a7d94a9d1cd29b11dca0a78
Anshikagupta1306 <anshika13064@gmail.com>
2025-02-18 10:30:30 +0530

    Add files via upload

---
+++ setithbit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class setithbit {
+    public static int setbit(int n,int i){
+        int bitmask=1<<i;
+        return n|bitmask;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=sc.nextInt();
+        System.out.println(setbit(n, i));
+    }
+}
