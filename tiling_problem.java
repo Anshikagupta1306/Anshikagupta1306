cf8272cc7c0abb164d8c161ce4d8efab051c4f13
Anshikagupta1306 <anshika13064@gmail.com>
2025-02-20 23:14:36 +0530

    Add files via upload

---
+++ tiling_problem.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class tiling_problem {
+    public static int tiling(int n){
+        if(n==0||n==1){
+            return 1;
+        }
+        int fnm1=tiling(n-1);
+        int fnm2=tiling(n-2);
+        return fnm1+fnm2;
+    }
+    public static void main(String[] args) {
+        System.out.println(tiling(4));
+    }
+}
